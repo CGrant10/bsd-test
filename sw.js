9ce3f70648c7ae1aff22900e99aa2c7ba65d7a21
CGrant10 <87659697+CGrant10@users.noreply.github.com>
2026-08-31 19:51:30 -0500

    Cache complete v1.2.0 PWA shell

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='bsd7-community-v1.1.3';
-const SHELL=['./','./index.html','./app.js','./config.js','./update.js','./manifest.json'];
+const CACHE='bsd7-community-v1.2.0';
+const SHELL=['./','./index.html','./app.js','./config.js','./update.js','./install.js','./install-ui.css','./manifest.json','./icons/app-192.svg','./icons/app-512.svg','./icons/apple-touch-icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('message',e=>{if(e.data==='SKIP_WAITING')self.skipWaiting()});
