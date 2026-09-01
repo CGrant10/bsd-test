b5d6284cc463e4508079a9a1e031dd55bfc002fc
CGrant10 <87659697+CGrant10@users.noreply.github.com>
2026-08-31 20:59:49 -0500

    Smooth and synchronize page navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bsd7-community-v1.3.0';
+const CACHE='bsd7-community-v1.3.1';
 const SHELL=['./','./index.html','./app.js','./config.js','./update.js','./install.js','./install-ui.css','./manifest.json','./icons/app-192.svg','./icons/app-512.svg','./icons/apple-touch-icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
